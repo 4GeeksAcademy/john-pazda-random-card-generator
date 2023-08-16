07d5bd824f3240706f348847c324a189a6aad01c
Jpazda <111332675+Jpazda@users.noreply.github.com>
2023-08-16 02:06:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "🤴",
+    "👸",
+    "🂻",
+    "A"
+  ];
+  let suits = ["❤️", "♠︎", "♣", "♦️"];
+
+  const randomValueGenerator = Math.floor(Math.random() * cardValues.length);
+  const randomSuitGenerator = Math.floor(Math.random() * suits.length);
+
+  let cardValue = document.querySelector(".values");
+  cardValue.textContent = cardValues[randomValueGenerator];
+
+  let topSuit = document.querySelector("#top");
+  let bottomSuit = document.querySelector("#bottom");
+
+  topSuit.textContent = suits[randomSuitGenerator];
+  bottomSuit.textContent = suits[randomSuitGenerator];
 };
